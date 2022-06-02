1fb32d5e4818107498ced9509ed481b02fdd0a01
thanhnth-0124 <nguyen.thi.hai.thanh@sun-asterisk.com>
2022-06-02 08:47:45 +0700

    change

--- server/index.js
+++ server/index.js
@@ -73,9 +73,9 @@ if (process.env.NODE_ENV === "production") {
   // app.get("*", (req, res) => {
   //   res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   // });
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+  // app.get("/*", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "index.html"));
+  // });
 }
 
 const port = process.env.PORT || 5000;

f00d05f127aced2df06cb1435cabcd1657350a69
thanhnth-0124 <nguyen.thi.hai.thanh@sun-asterisk.com>
2022-06-14 12:59:30 +0700

    test console.log cookies

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -10,7 +10,8 @@ const { getVolunteerByUserId } = require("../repository/volunteerRepository");
 
 let auth = (req, res, next) => {
   let token = req.cookies.w_auth;
-  console.log(req)
+  console.log(req.Cookies)
+  console.log(req.cookies)
 
   User.findByToken(token, (err, user) => {
     if (err) throw err;
